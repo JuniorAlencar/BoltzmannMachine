8caaf88b8f5f0587132899814375215f07dc0ad8
Júnior Alencar <junioralencar31415@gmail.com>
2024-11-18 14:52:42 -0300

    mensagem

--- BMProgramCarmona/python/err_observations.py
+++ BMProgramCarmona/python/err_observations.py
@@ -3,7 +3,7 @@ from src.err_observations_functions import *
 # Main
 if __name__ == "__main__":
     j = True
-
+    #ss
     arquivo_selecionado = selecionar_arquivo()
     if arquivo_selecionado:
         MCS, erro = carregar_dados(arquivo_selecionado, J=j)
